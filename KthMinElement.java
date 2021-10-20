0a3316f893b25cf363a8ed37f13c0e694fd60be9
komalkolani <72454798+komalkolani@users.noreply.github.com>
2021-10-20 20:17:54 +0530

    Update KthMinElement.java

--- KthMinElement.java
+++ KthMinElement.java
@@ -4,13 +4,9 @@ import java.io.*;
 
 class KthMin {
     public static void main(String[] args) throws IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 int t =  Integer.parseInt(br.readLine());
+		 int ar[]={2,5,7,3,15,25,10};	 
+	         int t =  Integer.parseInt(br.readLine());
 		 for(int j=0;j<t;j++) {
-			 int n =  Integer.parseInt(br.readLine());
-			 String[] st = br.readLine().split(" ");
-			 int target =  Integer.parseInt(br.readLine());
-			 int[] ar = new int[st.length];
 			 for(int i=0;i<ar.length;i++) {
 				 ar[i] = Integer.parseInt(st[i]);
 			 }
