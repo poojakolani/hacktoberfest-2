5b690e6efb1ac4519164e814bc74492d4f061fa2
komalkolani <72454798+komalkolani@users.noreply.github.com>
2021-10-21 17:17:19 +0530

    Update KthMinElement.java

--- KthMinElement.java
+++ KthMinElement.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class KthMin {
     public static void main(String[] args) throws IOException {
-		 int ar[]={2,5,7,3,15,25,10};	 
+		 int ar[]={2,5,7,3,15,25,10, 30,2};	 
 	         int t =  Integer.parseInt(br.readLine());
 		 for(int j=0;j<t;j++) {
 			 for(int i=0;i<ar.length;i++) {
@@ -13,5 +13,6 @@ class KthMin {
 			 Arrays.parallelSort(ar);
 			 System.out.println(ar[target-1]);
 		 }
+	    System.out.println("Task Accomplished");
 	}
 }
