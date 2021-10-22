5fba361c932f1477b9879694b10c4a756be13fa8
poojakolani <92969943+poojakolani@users.noreply.github.com>
2021-10-22 11:25:47 +0530

    Update MaxMin.java

--- MaxMin.java
+++ MaxMin.java
@@ -1,9 +1,11 @@
 class main{
     public static void main(String args[]) {
         int arr[] = {1000, 11, 445, 1, 330, 3000, 45,99,100};
+        int arr1[]={20,36,78,33,73}
         System.out.println(Arrays.max(arr));
         System.out.println(Arrays.min(arr));
-        System.out.println("Maximum and Minimum is found");
-
+        System.out.println(Arrays.max(arr1));
+        System.out.println(Arrays.min(arr2));
+        System.out.println("Task Accomplished!");
     }
 }
